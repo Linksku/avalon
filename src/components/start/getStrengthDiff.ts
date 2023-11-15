26a272c496f5b8fe59296564a65e45994d4b308d
Leo Jiang <leojiang000@gmail.com>
2023-11-15 00:46:47 -0800

    adjust strength diff for high player counts

--- src/components/start/getStrengthDiff.ts
+++ src/components/start/getStrengthDiff.ts
@@ -1,19 +1,22 @@
 const PLAYERS_TO_BASE_DIFF = new Map([
-  [5, 0.5],
-  [6, 1],
-  [7, 0],
-  [8, 0],
-  [9, 0.5],
+  [5, 1],
+  [6, 0],
+  [7, 0.5],
+  [8, -1],
+  [9, 1],
   [10, -1],
 ]);
 
 export default function getStrengthDiff(roles: Set<Role>) {
   const rolesArr = Array.from(roles).filter(r => r.name !== 'Drunk');
+  const baseDiff = PLAYERS_TO_BASE_DIFF.get(rolesArr.length) ?? 0
+    // Assumes players are uncoordinated and side convos will occur
+    - ((rolesArr.length / 5) - 1);
   const strengthDiff = Array.from(roles).reduce(
     (sum, role) => (role.isEvil
       ? sum - role.getStrength(rolesArr)
       : sum + role.getStrength(rolesArr)),
     0,
   );
-  return (PLAYERS_TO_BASE_DIFF.get(rolesArr.length) ?? 0) + strengthDiff;
+  return baseDiff + strengthDiff;
 }
