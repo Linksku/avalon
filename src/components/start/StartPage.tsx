a9ff588a7c54e1cce2ee817a11daab54c93293e1
Leo Jiang <leojiang000@gmail.com>
2024-01-19 01:08:38 -0800

    floating title

--- src/components/start/StartPage.tsx
+++ src/components/start/StartPage.tsx
@@ -6,8 +6,10 @@ import styles from './StartPage.module.scss';
 
 export default function StartPage() {
   return (
-    <div>
-      <h1 className={styles.title}>Avalon 2.0</h1>
+    <div className={styles.container}>
+      <div className={styles.titleWrap}>
+        <h1 className={styles.title}>Avalon 2.0</h1>
+      </div>
 
       <PlayersSelector />
 
